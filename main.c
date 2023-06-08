7dd13b940f41a813bb86df910afba2c388bc77a8
Teeratat Kraikuan <tkraikua@z3t4c4.42bangkok.com>
2023-06-08 17:55:23 +0700

    1

--- main.c
+++ main.c
@@ -6,14 +6,19 @@
 /*   By: tkraikua <tkraikua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 13:42:53 by tkraikua          #+#    #+#             */
-/*   Updated: 2023/06/08 14:12:52 by tkraikua         ###   ########.fr       */
+/*   Updated: 2023/06/08 17:54:31 by tkraikua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include "mlx.h"
 
-int main( void )
+int main()
 {
-	printf("Hello World\n");
-	return (0);
+    void *mlx = mlx_init();
+    void *win = mlx_new_window(mlx, 2000, 2000, "Tutorial Window - Draw Pixel");
+
+    mlx_pixel_put(mlx, win, 640/2, 360/2, 0xFFFFFF);
+
+    mlx_loop(mlx);
 }
